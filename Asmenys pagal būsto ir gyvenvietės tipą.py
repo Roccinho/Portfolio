9efe806c6d9071ec704d5c7c4fca5689acfa6a4d
Roccinho <73741424+Roccinho@users.noreply.github.com>
2021-07-27 18:51:05 +0300

    Update Asmenys pagal būsto ir gyvenvietės tipą.py

--- Asmenys pagal būsto ir gyvenvietės tipą.py
+++ Asmenys pagal būsto ir gyvenvietės tipą.py
@@ -19,9 +19,6 @@ bendras_2019=pd.merge (asm19, nu19, on='HB030')
 
 #kaimas name 2011
 
-#miestai=['Alytus', 'Kaunas', 'Klaipėda', 'Marijampolė', 'Panevėžys', 'Šiauliai', 'Tauragė', 
-#Telšiai', 'Utena', 'Vilnius']
-
 namaik11=bendras_2011[bendras_2011['PE040']==5].groupby(['HH010', 'M_K_y'])
 namai_kaime11=len(namaik11.get_group((1,2))['PB030'].values)
 butaik11=bendras_2011[bendras_2011['PE040']==5].groupby(['HH010', 'M_K_y'])
@@ -89,7 +86,6 @@ but_kaim_aukst19=np.append(butai_kaime19, butai_kaime19_mag)
 nam_miest_aukst19=np.append(namai_mieste19, namai_mieste19_mag)
 but_miest_aukst19=np.append(butai_mieste19, butai_mieste19_mag)
 
-
 fig, ax=plt.subplots()
 
 kategorijos=['Namai kaime', 'Butai kaime', 'Namai mieste', 'Butai mieste']
@@ -117,95 +113,3 @@ ax.bar_label(duomenys19, fontsize=11)
 
 fig.tight_layout()
 plt.show()
-
-
-# k_alytus_n11=bendras_2011[bendras_2011['AP_y']==1].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_alytus_n11=k_alytus_n11.get_group((5,1,2))['PB030'].values
-# k_kaunas_n11=bendras_2011[bendras_2011['AP_y']==2].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_kaunas_n11=k_kaunas_n11.get_group((5,1,2))['PB030'].values
-# k_klaipeda_n11=bendras_2011[bendras_2011['AP_y']==3].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_klaipeda_n11=k_klaipeda_n11.get_group((5,1,2))['PB030'].values
-# k_marijampole_n11=bendras_2011[bendras_2011['AP_y']==4].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_marijampole_n11=k_marijampole_n11.get_group((5,1,2))['PB030'].values
-# k_panevezys_n11=bendras_2011[bendras_2011['AP_y']==5].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_panevezys_n11=k_panevezys_n11.get_group((5,1,2))['PB030'].values
-# k_siauliai_n11=bendras_2011[bendras_2011['AP_y']==6].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_siauliai_n11=k_siauliai_n11.get_group((5,1,2))['PB030'].values
-# k_taurage_n11=bendras_2011[bendras_2011['AP_y']==7].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_taurage_n11=k_taurage_n11.get_group((5,1,2))['PB030'].values
-# k_telsiai_n11=bendras_2011[bendras_2011['AP_y']==8].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_telsiai_n11=k_telsiai_n11.get_group((5,1,2))['PB030'].values
-# k_utena_n11=bendras_2011[bendras_2011['AP_y']==9].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_utena_n11=k_utena_n11.get_group((5,1,2))['PB030'].values
-# k_vilnius_n11=bendras_2011[bendras_2011['AP_y']==10].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_vilnius_n11=k_vilnius_n11.get_group((5,1,2))['PB030'].values
-
-# # kaimas bute 2011
-
-# k_alytus_b11=bendras_2011[bendras_2011['AP_y']==1].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_alytus_b11=k_alytus_b11.get_group((5,4,2))['PB030'].values
-# k_kaunas_b11=bendras_2011[bendras_2011['AP_y']==2].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_kaunas_b11=k_kaunas_b11.get_group((5,4,2))['PB030'].values
-# k_klaipeda_b11=bendras_2011[bendras_2011['AP_y']==3].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_klaipeda_b11=k_klaipeda_b11.get_group((5,4,2))['PB030'].values
-# k_marijampole_b11=bendras_2011[bendras_2011['AP_y']==4].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_marijampole_b11=k_marijampole_b11.get_group((5,4,2))['PB030'].values
-# k_panevezys_b11=bendras_2011[bendras_2011['AP_y']==5].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_panevezys_b11=k_panevezys_b11.get_group((5,4,2))['PB030'].values
-# k_siauliai_b11=bendras_2011[bendras_2011['AP_y']==6].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_siauliai_b11=k_siauliai_b11.get_group((5,4,2))['PB030'].values
-# k_taurage_b11=bendras_2011[bendras_2011['AP_y']==7].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_taurage_b11=k_taurage_b11.get_group((5,4,2))['PB030'].values
-# k_telsiai_b11=bendras_2011[bendras_2011['AP_y']==8].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_telsiai_b11=k_telsiai_b11.get_group((5,4,2))['PB030'].values
-# k_utena_b11=bendras_2011[bendras_2011['AP_y']==9].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_utena_b11=k_utena_b11.get_group((5,4,2))['PB030'].values
-# k_vilnius_b11=bendras_2011[bendras_2011['AP_y']==10].groupby(['PE040', 'HH010', 'M_K_y',])
-# kaimas_vilnius_b11=k_vilnius_b11.get_group((5,4,2))['PB030'].values
-
-# #miestas name 
-
-# m_alytus_n11=bendras_2011[bendras_2011['AP_y']==1].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_alytus_n11=m_alytus_n11.get_group((5,1,1))['PB030'].values
-# m_kaunas_n11=bendras_2011[bendras_2011['AP_y']==2].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_kaunas_n11=m_kaunas_n11.get_group((5,1,1))['PB030'].values
-# m_klaipeda_n11=bendras_2011[bendras_2011['AP_y']==3].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_klaipeda_n11=m_klaipeda_n11.get_group((5,1,1))['PB030'].values
-# m_marijampole_n11=bendras_2011[bendras_2011['AP_y']==4].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_marijampole_n11=m_marijampole_n11.get_group((5,1,1))['PB030'].values
-# m_panevezys_n11=bendras_2011[bendras_2011['AP_y']==5].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_panevezys_n11=m_panevezys_n11.get_group((5,1,1))['PB030'].values
-# m_siauliai_n11=bendras_2011[bendras_2011['AP_y']==6].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_siauliai_n11=m_siauliai_n11.get_group((5,1,1))['PB030'].values
-# m_taurage_n11=bendras_2011[bendras_2011['AP_y']==7].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_taurage_n11=m_taurage_n11.get_group((5,1,1))['PB030'].values
-# m_telsiai_n11=bendras_2011[bendras_2011['AP_y']==8].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_telsiai_n11=m_telsiai_n11.get_group((5,1,1))['PB030'].values
-# m_utena_n11=bendras_2011[bendras_2011['AP_y']==9].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_utena_n11=m_utena_n11.get_group((5,1,1))['PB030'].values
-# m_vilnius_n11=bendras_2011[bendras_2011['AP_y']==10].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_vilnius_n11=m_vilnius_n11.get_group((5,1,1))['PB030'].values
-
-
-# #meistas bute 2011
-
-# m_alytus_b11=bendras_2011[bendras_2011['AP_y']==1].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_alytus_b11=m_alytus_b11.get_group((5,4,1))['PB030'].values
-# m_kaunas_b11=bendras_2011[bendras_2011['AP_y']==2].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_kaunas_b11=m_kaunas_b11.get_group((5,4,1))['PB030'].values
-# m_klaipeda_b11=bendras_2011[bendras_2011['AP_y']==3].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_klaipeda_b11=m_klaipeda_b11.get_group((5,4,1))['PB030'].values
-# m_marijampole_b11=bendras_2011[bendras_2011['AP_y']==4].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_marijampole_b11=m_marijampole_b11.get_group((5,4,1))['PB030'].values
-# m_panevezys_b11=bendras_2011[bendras_2011['AP_y']==5].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_panevezys_b11=m_panevezys_b11.get_group((5,4,1))['PB030'].values
-# m_siauliai_b11=bendras_2011[bendras_2011['AP_y']==6].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_siauliai_b11=m_siauliai_b11.get_group((5,4,1))['PB030'].values
-# m_taurage_b11=bendras_2011[bendras_2011['AP_y']==7].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_taurage_b11=m_taurage_b11.get_group((5,4,1))['PB030'].values
-# m_telsiai_b11=bendras_2011[bendras_2011['AP_y']==8].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_telsiai_b11=m_telsiai_b11.get_group((5,4,1))['PB030'].values
-# m_utena_b11=bendras_2011[bendras_2011['AP_y']==9].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_utena_b11=m_utena_b11.get_group((5,4,1))['PB030'].values
-# m_vilnius_b11=bendras_2011[bendras_2011['AP_y']==10].groupby(['PE040', 'HH010', 'M_K_y',])
-# miestas_vilnius_b11=m_vilnius_b11.get_group((5,4,1))['PB030'].values
